045a7cd1f79921b87acad00b92c3a320567c0b3e
fernando <fernandokojiyama@gmail.com>
2020-05-25 17:57:08 -0400

    add: webpack basic structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+import icon from './headIcon.png';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = icon;
+
+  element.appendChild(myIcon);
+
+
+  return element;
+}
+
+document.body.appendChild(component());
