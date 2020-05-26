69bb86eceee0a90172754654255225c6f4c5fc26
fernando <fernandokojiyama@gmail.com>
2020-05-25 21:41:42 -0400

    add: side menu structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { toolbarComponent } from './toolbar'
+import { toolbarComponent, sideMenu } from './toolbar'
 import icon from './assets/headIcon.png'
 
 document.getElementsByTagName('link')[0].href = icon;
 document.getElementById('toolbar').appendChild(toolbarComponent());
+document.body.append(sideMenu());
--- src/toolbar.js
+++ src/toolbar.js
@@ -1,21 +1,58 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
+import menuIcon from './assets/menu-24px.svg'
+
+var menu = {
+  state: false,
+  get text() {
+    return menu.state ?  "Aberto" : "Fechado";
+  },
+  get getState() { 
+    return this.state;
+  },
+ /*  set setState(x) {
+    this.state = x;
+  } */
+}
+
+function openSideMenu() {
+  document.getElementById("sideMenu").style["width"] = '30%';
+  menu.state =true
+  /* if(menu.getState) menu.state = false;
+  else menu.state =true; */
+}
+
+export function sideMenu() {
+  let sideMenu = document.createElement('div');
+  sideMenu.setAttribute("id", "sideMenu");
+  sideMenu.innerHTML = menu.text
+  sideMenu.classList.add("sideMenu");
+  sideMenu.style["width"] = 0;
+  sideMenu.onmouseleave = () => {
+    menu.state = false;
+    sideMenu.style["width"] = 0;
+  }
+  return sideMenu
+}
 
 export function toolbarComponent() {
-  const list = document.createElement('ul')
 
-  let item = document.createElement('li')
-  let seg = document.createElement('li')
-  item.innerHTML = "Menu"
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  list.classList.add('toolbar');
+  const myIcon = new Image();
+  myIcon.src = menuIcon;
+  let button = document.createElement('button');
+  button.classList.add('toolbar-button');
+  button.appendChild(myIcon);
+  button.onmouseover = openSideMenu;
 
-  // const myIcon = new Image();
-  // myIcon.src = icon;
+  let listElement = document.createElement('li');
+  listElement.appendChild(button);
+
+  let iconList = document.createElement('ul');
+  iconList.appendChild(listElement);
+  iconList.classList.add('toolbar');
+
   
-  seg.innerHTML = "segundo" 
-  list.appendChild(item);
-  list.appendChild(seg);
-
-  return list;
+  return iconList;
 }
+
+
