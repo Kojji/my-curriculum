78eae1d4ad266c890d38360b1dfaed22d38b6f68
fernando <fernandokojiyama@gmail.com>
2020-06-16 17:44:54 -0400

    add: menu icon position

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import { toolbarComponent, sideMenuComponent } from './toolbar'
 import icon from './assets/headIcon.png'
 import { footerText } from './footer'
+import menuIcon from './assets/menu-24px.svg'
 
-// document.getElementsByTagName('link')[0].href = icon;
+document.getElementsByTagName('link')[0].href = icon;
 // document.getElementById('toolbar').appendChild(toolbarComponent());
 // document.body.appendChild(sideMenuComponent());
 document.getElementById('footer-text').innerHTML = footerText();
+
+
+const myIcon = new Image();
+myIcon.src = menuIcon;
+let menuButton = document.createElement('button');
+menuButton.classList.add('toolbar-button');
+menuButton.appendChild(myIcon);
+//menuButton.onmouseover = openSideMenu;
+document.getElementById('menu-icon').appendChild(menuButton);
