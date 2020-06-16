a3aa88114638ed0bb8b4ee33819514e79993a320
fernando <fernandokojiyama@gmail.com>
2020-06-16 16:31:19 -0400

    add: grid layout structure

---
+++ src/footer.js
@@ -0,0 +1,4 @@
+export function footerText() {
+  let date = new Date();
+  return `${date.getFullYear()} - Fernando Koji`
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { toolbarComponent, sideMenuComponent } from './toolbar'
 import icon from './assets/headIcon.png'
+import { footerText } from './footer'
 
-document.getElementsByTagName('link')[0].href = icon;
-document.getElementById('toolbar').appendChild(toolbarComponent());
-document.body.appendChild(sideMenuComponent());
+// document.getElementsByTagName('link')[0].href = icon;
+// document.getElementById('toolbar').appendChild(toolbarComponent());
+// document.body.appendChild(sideMenuComponent());
+document.getElementById('footer-text').innerHTML = footerText();
