cba4269060220846848b8f58a7c37f122137a94d
fernando <fernandokojiyama@gmail.com>
2020-06-27 20:51:22 -0400

    add: simple drop animation

--- src/js/animationBanner.js
+++ src/js/animationBanner.js
@@ -1,3 +1,5 @@
+import brasil from '../assets/bandeira.png';
+
 var animationBanner = document.getElementById("animation-banner");
 var hoverAnimationBanner = document.getElementById("hover-animation-banner");
 var hoverContent = document.getElementById("hover-banner-content");
@@ -17,6 +19,12 @@ function setContent() {
   let myJob = document.createElement('span');
   myJob.innerHTML = 'Programador Web'
   myJob.style['fontSize'] = '2vw';
+  myJob.style["margin-bottom"] = '5px';
+  let brasilFlag = document.createElement('img');
+  brasilFlag.src = brasil;
+  brasilFlag.style["width"] = "1.2vw"
+  brasilFlag.style["height"] = "auto";
+  brasilFlag.style["margin-right"] = "4px";
   let myLocal = document.createElement('span');
   myLocal.innerHTML = 'Brasil, Dourados - MS'
   myLocal.style['fontSize'] = '1.2vw';
@@ -25,6 +33,7 @@ function setContent() {
   hoverContent.appendChild(document.createElement('br'));
   hoverContent.appendChild(myJob);
   hoverContent.appendChild(document.createElement('br'));
+  hoverContent.appendChild(brasilFlag);
   hoverContent.appendChild(myLocal);
 }
 
