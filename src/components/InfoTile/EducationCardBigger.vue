7273a70c01e345a45f68720180bf1e571da2ad11
Kojji <fernandokojiyama@gmail.com>
2022-06-13 01:19:32 -0300

    add: cards shadow

--- src/components/InfoTile/EducationCardBigger.vue
+++ src/components/InfoTile/EducationCardBigger.vue
@@ -4,12 +4,14 @@
 		<hr />
 		<div class="row">
 			<div id="experience-card-list" class="list-group col-4">
-				<h4 class="mt-3">{{$t('InfoCards.Education.TitleFirst')}}</h4>
-				<a class="list-group-item list-group-item-action" href="#list-item-1">{{$t('InfoCards.Education.ItemTitleOne')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-2">{{$t('InfoCards.Education.ItemTitleTwo')}}</a>
-				<h4 class="mt-3">{{$t('InfoCards.Education.TitleSecond')}}</h4>
-				<a class="list-group-item list-group-item-action" href="#list-item-3">{{$t('InfoCards.Education.ItemTitleThree')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-4">{{$t('InfoCards.Education.ItemTitleFour')}}</a>
+				<div class="icon-box">
+					<h4 class="mt-3">{{$t('InfoCards.Education.TitleFirst')}}</h4>
+					<a class="list-group-item list-group-item-action" href="#list-item-1">{{$t('InfoCards.Education.ItemTitleOne')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-2">{{$t('InfoCards.Education.ItemTitleTwo')}}</a>
+					<h4 class="mt-3">{{$t('InfoCards.Education.TitleSecond')}}</h4>
+					<a class="list-group-item list-group-item-action" href="#list-item-3">{{$t('InfoCards.Education.ItemTitleThree')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-4">{{$t('InfoCards.Education.ItemTitleFour')}}</a>
+				</div>
 			</div>
 			<div data-spy="scroll" data-target="#experience-card-list" data-offset="20" class="scrollspy-example col-8" id="content">
 				<h4 class="mt-3 mb-2">{{$t('InfoCards.Education.TitleFirst')}}</h4>
@@ -42,5 +44,9 @@ export default {
 .content-item{
   font-size: 18px;
 	text-align: justify;
+}
+.icon-box{
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 0.5rem;
 }
 </style>
--- src/components/InfoTile/ExperienceCardBigger.vue
+++ src/components/InfoTile/ExperienceCardBigger.vue
@@ -4,23 +4,24 @@
 		<hr />
 		<div class="row">
 			<div id="experience-card-list" class="list-group col-4">
-				<h4 class="mt-3">{{$t('InfoCards.Experience.SectionOne')}}</h4>
-				<a class="list-group-item list-group-item-action" href="#list-item-13">{{$t('InfoCards.Experience.TitleThirteenth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-12">{{$t('InfoCards.Experience.TitleTwelveth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-1">{{$t('InfoCards.Experience.TitleFirst')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-2">{{$t('InfoCards.Experience.TitleSecond')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-3">{{$t('InfoCards.Experience.TitleThird')}}</a>
-				<h4 class="mt-3">{{$t('InfoCards.Experience.SectionTwo')}}</h4>
-				<a class="list-group-item list-group-item-action" href="#list-item-4">{{$t('InfoCards.Experience.TitleFourth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-5">{{$t('InfoCards.Experience.TitleFifth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-6">{{$t('InfoCards.Experience.TitleSixth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-7">{{$t('InfoCards.Experience.TitleSeventh')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-8">{{$t('InfoCards.Experience.TitleEigth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-9">{{$t('InfoCards.Experience.TitleNineth')}}</a>
-				<h4 class="mt-3">{{$t('InfoCards.Experience.SectionThree')}}</h4>
-				<a class="list-group-item list-group-item-action" href="#list-item-10">{{$t('InfoCards.Experience.TitleTenth')}}</a>
-				<a class="list-group-item list-group-item-action" href="#list-item-11">{{$t('InfoCards.Experience.TitleEleventh')}}</a>
-
+				<div class="icon-box">
+					<h4 class="mt-3">{{$t('InfoCards.Experience.SectionOne')}}</h4>
+					<a class="list-group-item list-group-item-action" href="#list-item-13">{{$t('InfoCards.Experience.TitleThirteenth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-12">{{$t('InfoCards.Experience.TitleTwelveth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-1">{{$t('InfoCards.Experience.TitleFirst')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-2">{{$t('InfoCards.Experience.TitleSecond')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-3">{{$t('InfoCards.Experience.TitleThird')}}</a>
+					<h4 class="mt-3">{{$t('InfoCards.Experience.SectionTwo')}}</h4>
+					<a class="list-group-item list-group-item-action" href="#list-item-4">{{$t('InfoCards.Experience.TitleFourth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-5">{{$t('InfoCards.Experience.TitleFifth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-6">{{$t('InfoCards.Experience.TitleSixth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-7">{{$t('InfoCards.Experience.TitleSeventh')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-8">{{$t('InfoCards.Experience.TitleEigth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-9">{{$t('InfoCards.Experience.TitleNineth')}}</a>
+					<h4 class="mt-3">{{$t('InfoCards.Experience.SectionThree')}}</h4>
+					<a class="list-group-item list-group-item-action" href="#list-item-10">{{$t('InfoCards.Experience.TitleTenth')}}</a>
+					<a class="list-group-item list-group-item-action" href="#list-item-11">{{$t('InfoCards.Experience.TitleEleventh')}}</a>
+				</div>
 			</div>
 			<div data-spy="scroll" data-target="#experience-card-list" data-offset="20" class="scrollspy-example col-8">
 				<h4 class="mt-3 mb-2">{{$t('InfoCards.Experience.SectionOne')}}</h4>
@@ -73,5 +74,10 @@ export default {
 .content-item{
 	font-size: 18px;
 	text-align: justify;
+}
+
+.icon-box{
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 0.5rem;
 }
 </style>
